9c80c2bcccb751298e10a860cfb408f40c5a97a6
Oliver Tušla <oliver.tusla@seznam.cz>
2023-06-23 19:24:51 +0200

    basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,26 @@
-fn main() {
+use clap::Parser;
+use lexer::Lexer;
+use std::path::PathBuf;
+
+mod lexer;
+
+#[derive(Parser)]
+#[command(arg_required_else_help = true)]
+/// Mila compiler
+struct Cli {
+    /// Script file to execute
+    file: PathBuf,
+
+    #[arg(long)]
+    /// Dump tokenized input
+    dump_tokens: bool,
+}
+
+fn main() -> Result<(), String> {
+    let cli = Cli::parse();
+    let tokens = Lexer::tokenize_file(cli.file)?;
+    if cli.dump_tokens {
+        println!("{:#?}", tokens);
+    }
+    Ok(())
 }
