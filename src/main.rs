062743dfa98ca08660ff43778f9c9921c2c74464
Oliver Tušla <oliver.tusla@seznam.cz>
2023-06-25 00:21:51 +0200

    add parser to the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use clap::Parser;
+use clap::Parser as ClapParser;
 use lexer::Lexer;
+use parser::Parser;
 use std::path::PathBuf;
 
 mod lexer;
+mod parser;
 
-#[derive(Parser)]
+#[derive(ClapParser)]
 #[command(arg_required_else_help = true)]
 /// Mila compiler
 struct Cli {
     /// Script file to execute
     file: PathBuf,
+
+    #[arg(long)]
+    /// Dump AST of the input
+    dump_ast: bool,
 
     #[arg(long)]
     /// Dump tokenized input
@@ -21,6 +27,10 @@ fn main() -> Result<(), String> {
     let tokens = Lexer::tokenize_file(cli.file)?;
     if cli.dump_tokens {
         println!("{:#?}", tokens);
+    }
+    let ast = Parser::new(&tokens).parse();
+    if cli.dump_ast {
+        println!("{:#?}", ast);
     }
     Ok(())
 }
