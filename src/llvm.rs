af03e5e7c5635c238a7e8074d1481f36a3a88597
Oliver Tušla <oliver.tusla@seznam.cz>
2023-06-27 10:50:56 +0200

    lower constants and variables

--- src/llvm.rs
+++ src/llvm.rs
@@ -1,13 +1,16 @@
-use crate::parser::{
-    ArrayType, Block, Declaration, FunctionDeclaration, ProcedureDeclaration, Program, SimpleType,
-    Statement, Type,
+use crate::{
+    lexer::Constant,
+    parser::{
+        ArrayType, Block, Declaration, Expression, FunctionDeclaration, ProcedureDeclaration,
+        Program, SimpleType, Statement, Type,
+    },
 };
 use inkwell::{
     builder::Builder,
     context::Context,
     module::Module,
     types::{BasicType, BasicTypeEnum},
-    values::{BasicValue, FunctionValue, PointerValue},
+    values::{BasicValueEnum, FunctionValue, PointerValue},
 };
 use std::collections::HashMap;
 
@@ -116,13 +119,88 @@ impl<'a> LLVMGenerator<'a> {
         self.statement(block.body)
     }
 
+    fn create_alloca(&mut self, name: &str, typ: Type) -> PointerValue<'a> {
+        if self.symbol_table.is_global_scope() {
+            // TODO should initialize??
+            self.module
+                .add_global(self.r#type(typ), None, name)
+                .as_pointer_value()
+        } else {
+            let fn_value = self.module.get_last_function().unwrap();
+            let entry = fn_value.get_first_basic_block().unwrap();
+            let builder = self.context.create_builder();
+            match entry.get_first_instruction() {
+                Some(first_instr) => builder.position_before(&first_instr),
+                None => builder.position_at_end(entry),
+            }
+            match typ {
+                Type::Array(ArrayType {
+                    range,
+                    element_type,
+                }) => builder.build_array_alloca(
+                    self.r#type(Type::Simple(element_type)),
+                    self.context
+                        .i64_type()
+                        .const_int(range.count() as u64, false),
+                    &name,
+                ),
+                t @ Type::Simple(_) => builder.build_alloca(self.r#type(t), name),
+            }
+        }
+    }
+
     fn declarations(&mut self, declarations: Vec<Declaration>) -> GeneratorResult<()> {
         for decl in declarations {
             match decl {
-                Declaration::Constants(_) => todo!(),
+                Declaration::Constants(consts) => {
+                    for (name, expr) in consts {
+                        let val = self.expression(expr)?;
+                        let typ = match val {
+                            BasicValueEnum::ArrayValue(_) => todo!(),
+                            BasicValueEnum::IntValue(_) => Type::Simple(SimpleType::Integer),
+                            BasicValueEnum::FloatValue(_) => Type::Simple(SimpleType::Double),
+                            _ => Err("Unexpected expression type in constant declaration.")?,
+                        };
+                        let ptr = self.create_alloca(&name, typ.clone());
+                        self.builder.build_store(ptr, val);
+                        let symbol_info = SymbolInfo {
+                            is_mutable: false,
+                            r#type: typ,
+                            ptr,
+                        };
+                        self.symbol_table
+                            .insert(name.clone(), symbol_info)
+                            .map_err(|err| match err {
+                                TableError::AlreadyExists => {
+                                    format!("Variable '{}' has been already declared.", name)
+                                }
+                                TableError::NoScopeExists => {
+                                    "No scope has been created.".to_string()
+                                }
+                            })?;
+                    }
+                }
                 Declaration::Function(_) => todo!(),
                 Declaration::Procedure(_) => todo!(),
-                Declaration::Variables(_) => todo!(),
+                Declaration::Variables(vars) => {
+                    for (name, typ) in vars {
+                        let symbol_info = SymbolInfo {
+                            is_mutable: true,
+                            ptr: self.create_alloca(name.as_str(), typ.clone()),
+                            r#type: typ,
+                        };
+                        self.symbol_table
+                            .insert(name.clone(), symbol_info)
+                            .map_err(|err| match err {
+                                TableError::AlreadyExists => {
+                                    format!("Variable '{}' has been already declared.", name)
+                                }
+                                TableError::NoScopeExists => {
+                                    "No scope has been created.".to_string()
+                                }
+                            })?;
+                    }
+                }
             }
         }
         Ok(())
@@ -163,7 +241,37 @@ impl<'a> LLVMGenerator<'a> {
         }
     }
 
-    fn r#type(&self, t: Type) -> BasicTypeEnum {
+    fn expression(&mut self, expression: Expression) -> GeneratorResult<BasicValueEnum<'a>> {
+        Ok(match expression {
+            Expression::ArrayAccess { array_name, index } => todo!(),
+            Expression::Constant(c) => match c {
+                // Yes, casting is the right option..
+                // https://github.com/llvm/llvm-project/blob/llvmorg-15.0.7/llvm/lib/Support/APInt.cpp#L2088
+                Constant::Integer(int) => {
+                    self.context.i64_type().const_int(int as u64, false).into()
+                }
+                Constant::Double(dbl) => self.context.f64_type().const_float(dbl).into(),
+                Constant::String(string) => {
+                    self.context.const_string(string.as_bytes(), false).into()
+                }
+            },
+            Expression::BinaryOperation {
+                operator,
+                left,
+                right,
+            } => todo!(),
+            Expression::FunctionCall {
+                function_name,
+                arguments,
+            } => todo!(),
+            Expression::Variable(name) => match self.symbol_table.find(&name) {
+                Some(SymbolInfo { ptr, .. }) => (*ptr).into(),
+                None => Err(format!("Undefined variable '{}'.", name))?,
+            },
+        })
+    }
+
+    fn r#type(&self, t: Type) -> BasicTypeEnum<'a> {
         match t {
             Type::Array(ArrayType {
                 range,
