137770e3cd5dbd1f8d4ae9087bfdc70d8880cb5c
Oliver Tušla <oliver.tusla@seznam.cz>
2023-06-30 00:07:52 +0200

    io flush

--- stdlib/src/lib.rs
+++ stdlib/src/lib.rs
@@ -1,8 +1,12 @@
-use std::ffi::{c_char, CStr};
+use std::{
+    ffi::{c_char, CStr},
+    io::{stdout, Write},
+};
 
 #[no_mangle]
 pub extern "C" fn mila_write(int: i64) {
     print!("{}", int);
+    let _ = stdout().flush();
 }
 
 #[no_mangle]
@@ -13,6 +17,7 @@ pub extern "C" fn mila_writeln(int: i64) {
 #[no_mangle]
 pub extern "C" fn mila_dbl_write(double: f64) {
     print!("{}", double);
+    let _ = stdout().flush();
 }
 
 #[no_mangle]
@@ -24,6 +29,7 @@ pub extern "C" fn mila_dbl_writeln(double: f64) {
 pub extern "C" fn mila_str_write(ptr: *const c_char) {
     unsafe {
         print!("{}", CStr::from_ptr(ptr).to_str().unwrap());
+        let _ = stdout().flush();
     }
 }
 
